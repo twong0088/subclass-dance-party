677171ff30fbd2d5dd3d28d407429ad5d1c0ff78
markwgquintos <markwgquintos@yahoo.com>
2020-10-13 14:50:42 -0700

    Fixed blinky to rotate

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -2,14 +2,15 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('blinkyDancer');
+  this.$node.append("<img id='blinky' src='pic/cyan.png'/>");
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function () {
-  makeDancer.prototype.step.call(this);
-  this.$node.toggle();
+  // makeDancer.prototype.step.call(this);
+  // this.$node.toggle();
 };
 
 makeBlinkyDancer.prototype.groupUp = function() {
